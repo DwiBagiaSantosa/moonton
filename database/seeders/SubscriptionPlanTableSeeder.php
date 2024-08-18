58e7411724fd8e238ace3b8cf3ec06c5986f7b51
Dwi Bagia Santosa <dwibagiasentosa@gmail.com>
2024-08-18 14:45:27 +0700

    feat: migrations and seeders

---
+++ database/seeders/SubscriptionPlanTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\SubscriptionPlan;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SubscriptionPlanTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $subscriptionPlans = [
+            [
+                'name' => 'Basic',
+                'price' => 200000,
+                'active_period_in_months' => 3,
+                'features' => json_encode(['Basic 1', 'Basic 2', 'Basic 3'])
+            ],
+            [
+                'name' => 'Premium',
+                'price' => 800000,
+                'active_period_in_months' => 6,
+                'features' => json_encode(['Basic 1', 'Basic 2', 'Basic 3', 'Basic 5', 'Basic 5'])
+            ],
+        ];
+
+        SubscriptionPlan::insert($subscriptionPlans);
+    }
+}
